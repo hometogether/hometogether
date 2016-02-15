131780ee6844800e8d50c58ff8e18e2f1ecdf783
Hometogether <hometogether92@gmail.com>
2016-02-15 16:44:15 +0100

    Gestione interessi completata. Inizializzazione del Contesto dell'applicazione con la lista dei Comuni all'interno.

--- EnterpriseApplication1-war/web/js/interessi.js
+++ EnterpriseApplication1-war/web/js/interessi.js
@@ -13,8 +13,8 @@ function aggiungiInteresse() {
             console.log("entro nel trim");
             //jQuery.noConflict();
             $("#nomeinteresse").val('');
-
-            $('#ulInteressi').append('<li id="'+id+'"><div class="btn-group"><form action="InterestServlet" role="form" method="post"><button class="btn-secondary borderless-btn" style="text-align: center;color: black">' + nomeinteresse + '</button><button id="remove" type="button" class="btn-secondary close" onClick="rimuoviInteresse(' + id + ')">&times;</button></form></div></li>');
+            var lowerinteressi = nomeinteresse.toLowerCase();
+            $('#ulInteressi').append('<li id="'+id+'"><div class="btn-group"><form action="InterestServlet" role="form" method="post"><button class="btn-secondary borderless-btn" style="text-align: center;color: black">' + lowerinteressi + '</button><button id="remove" type="button" class="btn-secondary close" onClick="rimuoviInteresse(' + id + ')">&times;</button></form></div></li>');
 
 
         } else {
